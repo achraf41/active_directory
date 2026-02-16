484a6f3d4c94d571d46cfe19a314d7893888862f
achraf41 <achraf.snoussi.etu24@ensem.ac.ma>
2026-02-16 00:18:24 +0000

    AD automation

---
+++ code/gen_ad.ps1
@@ -0,0 +1,47 @@
+param( [Parameter(Mandatory=$true)] $JSONFile )
+
+function CreateADGroup() {
+    param( [Parameter(Mandatory=$true)] $groupObject )
+    
+    $name = $groupObject.name
+    New-ADGroup -name $name -GroupScope Global
+
+}
+
+function CreateADUser() {
+    param( [Parameter(Mandatory=$true)] $userObject )
+
+    $name = $userObject.name
+    $password = $userObject.password
+
+    $firstname, $lastname = $name.Split(" ")
+    $username = ($firstname[0] + $lastname).ToLower()
+    $samAccountName = $username
+    $principalname = $username
+
+
+    New-ADuser -Name $name -GivenName $firstname -Surname $lastname -SamAccountName $samAccountName -UserPrincipalName $principalname@Global:Domain -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -PassThru | Enable-ADAccount
+
+    foreach($group_name in $userObject.groups){
+        try{
+            Get-ADGroup -Identity "$group_name"
+            Add-ADGroupMember -Identity $group_name -Members $username
+        }
+        catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+        {
+            Write-Warning "User $name NOT added to group $group_name because it does not exist"
+        }
+    }
+    
+}
+
+$json = ( Get-Content $JSONFile | ConvertFrom-JSON)
+
+$Global:Domain = $json.domain 
+
+foreach ( $group in $json.groups ){
+    CreateADGroup $group
+}
+foreach ( $user in $json.users ){
+    CreateADUser $user
+}
